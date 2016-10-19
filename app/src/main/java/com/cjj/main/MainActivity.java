7a09982392083be988ae2bfdf6c4b3d072e4fa8b
cjj <chenjunjun@haizitong.com>
2016-10-19 09:03:42 +0800

    test

--- app/src/main/java/com/cjj/main/MainActivity.java
+++ app/src/main/java/com/cjj/main/MainActivity.java
@@ -3,12 +3,16 @@ package com.cjj.main;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 程序主页
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
     }
+
+
 }
